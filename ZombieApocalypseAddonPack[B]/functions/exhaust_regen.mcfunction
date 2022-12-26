aff0b2bf759646c0eedbab9a105dc238eb61d91c
Artie Trey <58829269+Inshomothal@users.noreply.github.com>
2022-12-26 03:39:30 -0800

    Local Changes

--- ZombieApocalypseAddonPack[B]/functions/exhaust_regen.mcfunction
+++ ZombieApocalypseAddonPack[B]/functions/exhaust_regen.mcfunction
@@ -1,4 +1,3 @@
-say debug exhuast
 scoreboard objectives add exhregen dummy
 scoreboard players add @a[scores={exhregen=..39}] exhregen 1
 scoreboard players add @a[scores={exhregen=40, exhaust=..149}] exhaust 1
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
+const { src, dest } = require("gulp");
+const babel = require("gulp-babel");
+
 function defaultTask(cb) {
-  //place code for your default task here
+  // place code for your default task here
   cb();
 }
 
-exports.default = defaultTask;
+exports.default = function () {
+  return src("src/*.js").pipe(babel()).pipe(dest("output/"));
+};
